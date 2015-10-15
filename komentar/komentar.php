c46306c09f3f9f37d1683481cf61c7439154adc6
cecep <cecepefendi.c2p@gmail.com>
2015-10-15 16:00:52 +0700

    membuat komentar di php

---
+++ komentar/komentar.php
@@ -0,0 +1,14 @@
+<?php 
+
+	//echo digunakan untuk menampilkan ke browser
+	echo 'Belajar php <br />';
+
+	#ini juga komentar
+	echo "Belajar php kutip dua";
+
+	/*
+		ini komentar untuk
+		komentar yg panjang
+	*/
+	echo '<h1>Ini judul</h1>';
+?>
