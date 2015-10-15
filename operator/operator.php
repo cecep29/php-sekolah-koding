bce06f8b338c8e74ad528c82b3f01d01247aa627
cecep <cecepefendi.c2p@gmail.com>
2015-10-15 16:26:02 +0700

    operator di php

---
+++ operator/operator.php
@@ -0,0 +1,24 @@
+<?php 
+
+	$x = 10;
+	$y = 5;
+	//operator penambahan
+	echo $x + $y . "<br/>";
+	//operator pengurangan
+	echo $x - $y . "<br/>";
+	//operator perkalian
+	echo $x * $y . "<br/>";
+	//operator pembagian
+	echo $x / $y . "<br/>";
+	//operator modulus atau sisa bagi
+	echo $x % $y . "<br/>";
+
+	++$x;
+	echo $x . "<br/>";
+
+	$x++;
+	echo $x . "<br/>";
+
+	$x += 20;
+	echo $x . "<br/>";
+?>
