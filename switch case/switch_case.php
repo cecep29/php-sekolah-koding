0a49175907095e6990e51500026f0af6d687e9d7
cecep <cecepefendi.c2p@gmail.com>
2015-10-19 09:31:54 +0700

    swtich case di php

---
+++ switch case/switch_case.php
@@ -0,0 +1,17 @@
+<?php 
+
+	//switch case
+	$x = 3;
+
+	switch($x){
+		case 0 :
+		echo 'nilainya 0';
+		break;
+		case 1 :
+		echo 'nilainya 1';
+		break;
+		default:
+		echo 'tidak memenuhi';
+	}
+
+?>
