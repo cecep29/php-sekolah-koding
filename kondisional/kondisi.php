9da2612a8def1cb62f0a7295ee9c5841ad1588b6
cecep <cecepefendi.c2p@gmail.com>
2015-10-19 08:41:34 +0700

    kondisional if else di php

---
+++ kondisional/kondisi.php
@@ -0,0 +1,15 @@
+<?php 
+
+	$x =10;
+
+	//if( syarat ) maka {} else {}
+	if($x > 10){
+		echo 'Angka > 10';
+	}else if($x == 10){
+		echo 'angka 10';
+	}
+	else {
+		echo 'bukan angka 10';
+	}
+
+?>
