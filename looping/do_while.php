f9bf06a02c40ca0eda6acb5d44aa49eff35cf39e
cecep <cecepefendi.c2p@gmail.com>
2015-10-19 09:58:44 +0700

    looping di php

---
+++ looping/do_while.php
@@ -0,0 +1,12 @@
+<?php 
+
+//loop: do while
+/*perbedaan dengan while adalah min 1 statement akan dieksekusi 
+walaupun kondisi while-nya salah/false*/
+	$x = 0;
+	do {
+		echo $x;
+		$x++;
+	}while( $x<=10 );
+
+?>
---
+++ looping/while.php
@@ -0,0 +1,10 @@
+<?php 
+
+	//loop: while
+	$x = 0;
+	while($x <= 10 ){
+		echo $x;
+		$x++;
+	}
+
+?>
