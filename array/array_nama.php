eda3ad8df17038d3efc62e006d808e56c9aa4439
cecep <cecepefendi.c2p@gmail.com>
2015-10-19 11:21:40 +0700

    mencetak array di php

---
+++ array/array_nama.php
@@ -0,0 +1,12 @@
+<?php 
+
+	//array
+	$nama = ["Cecep", "Effi", "Razqa"];
+
+	//mencetak array
+	//count(var) untuk menghitung jumlah array
+	for($x=0; $x < count($nama); $x++){
+		echo $nama[$x] . " ";
+	}
+
+?>
