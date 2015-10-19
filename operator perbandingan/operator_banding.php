10c0f3bc3193f26702ac9fe00631a88c61ba7191
cecep <cecepefendi.c2p@gmail.com>
2015-10-19 09:05:20 +0700

    operator perbandingan di php

---
+++ operator perbandingan/operator_banding.php
@@ -0,0 +1,20 @@
+<?php 
+
+//operator perbandingan
+	$x = 10;
+	$y = "10";
+	$z = 5;
+
+	// === membandingkan tipe data dan nilai
+	if($x === $y){
+		echo 'tipe data tidak sama';
+	 }
+	 // == membandingkan nilai 
+		//else if($x == $y){
+	// 	echo 'nilai sama';
+	// }
+	else {
+		echo 'nilai tidak sama';
+	}
+
+?>
